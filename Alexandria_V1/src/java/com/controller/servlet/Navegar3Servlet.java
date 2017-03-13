605fbc6fa21def0e8a499ac4611a4b8388607055
AbrilAbitia <yoru@outlook.com>
2017-03-13 02:21:17 -0600

    Navegar3Servlet PDF.

--- Alexandria_V1/src/java/com/controller/servlet/Navegar3Servlet.java
+++ Alexandria_V1/src/java/com/controller/servlet/Navegar3Servlet.java
@@ -8,7 +8,7 @@ package com.controller.servlet;
 import com.model.dao.MaterialDAO;
 import com.model.entities.Material;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,8 +22,7 @@ import javax.servlet.http.HttpSession;
 public class Navegar3Servlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      *
      * @param request servlet request
      * @param response servlet response
@@ -31,53 +30,38 @@ public class Navegar3Servlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Navegar3Servlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Navegar3Servlet at " + request.getContextPath() + "</h1>");
-            
-            
-            HttpSession session = request.getSession();
-            
-            String idtip;
-            
-            int idMaterial = Integer.parseInt(request.getParameter("IdMaterial"));
-            System.out.println("\n\t" + idMaterial + " ");
-            Material m1 = new Material();
-            Material m2 = new Material();
-            MaterialDAO mdao1 = new MaterialDAO();
-            
-            int idtipom;
-            m1.setIdMaterial(idMaterial);
-            
-            m2 = mdao1.read(m1);
-            idtipom = m2.getTipoMaterial();
-            
-            if (idtipom == 1) {
-                idtip = Integer.toString(idMaterial);
-                System.out.println("Dato enviado ID: " + idtip);
-                session.setAttribute("idMaterial", idtip);
-                response.sendRedirect("jsp/alumno/vervideo.jsp");
-            }
-            else if (idtipom == 0){
-                idtip = Integer.toString(idMaterial);
-                System.out.println("Dato enviado ID: " + idtip);
-                session.setAttribute("idMaterial", idtip);
-                response.sendRedirect("jsp/alumno/verpdf.jsp");
-            }
-            
-            
-            
-            out.println("</body>");
-            out.println("</html>");
-        }
+	    throws ServletException, IOException {
+	response.setContentType("text/html;charset=UTF-8");
+	try (OutputStream out = response.getOutputStream()) {
+	    HttpSession session = request.getSession();
+	    String idtip;
+	    int idMaterial = Integer.parseInt(request.getParameter("IdMaterial"));
+	    System.out.println("\n\t" + idMaterial + " ");
+	    Material m1 = new Material();
+	    Material m2 = new Material();
+	    MaterialDAO mdao1 = new MaterialDAO();
+	    int idtipom;
+	    m1.setIdMaterial(idMaterial);
+	    m2 = mdao1.read(m1);
+	    idtipom = m2.getTipoMaterial();
+	    if (idtipom == 1) {
+		idtip = Integer.toString(idMaterial);
+		System.out.println("Dato enviado ID: " + idtip);
+		session.setAttribute("idMaterial", idtip);
+		response.sendRedirect("jsp/alumno/vervideo.jsp");
+	    } else if (idtipom == 0) {
+		byte[] bytes;
+		if ((bytes = mdao1.getPDF(m2)) != null) {
+		    String pdfFileName = m2.getNombreMaterial() + ".pdf";
+		    response.setContentType("application/pdf");
+		    response.addHeader("Content-Disposition", "attachment; filename=" + pdfFileName);
+		    response.setContentLength(bytes.length);
+		    out.write(bytes);
+		} else {
+		    System.out.println("PDF es nulo.");
+		}
+	    }
+	}
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -91,8 +75,8 @@ public class Navegar3Servlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+	    throws ServletException, IOException {
+	processRequest(request, response);
     }
 
     /**
@@ -105,8 +89,8 @@ public class Navegar3Servlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+	    throws ServletException, IOException {
+	processRequest(request, response);
     }
 
     /**
@@ -116,7 +100,7 @@ public class Navegar3Servlet extends HttpServlet {
      */
     @Override
     public String getServletInfo() {
-        return "Short description";
+	return "Short description";
     }// </editor-fold>
 
 }
